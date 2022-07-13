81566d51cad727c946198e637ef814afa2dd80bf
ginabeki <ginabeki3@gmail.com>
2022-07-13 21:27:08 +0300

    update script

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-// Js script goes here
+let playerScore = 0;
+let computerScore = 0;
+let roundWinner = "";
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    roundWinner = "tie";
+  }
+  if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK")
+  ) {
+    playerScore++;
+    roundWinner = "player";
+  }
+  if (
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER") ||
+    (computerSelection === "PAPER" && playerSelection === "ROCK")
+  ) {
+    computerScore++;
+    roundWinner = "computer";
+  }
+  updateScoreMessage(roundWinner, playerSelection, computerSelection);
+}
+
+function randomChoice() {
+  let randomNUm = Math.floor(Math.random() * 3);
+  switch (randomNUm) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
+}
+
+function gameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
